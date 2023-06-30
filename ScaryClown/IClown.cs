aa36fc4022d26b8d9c1301e8148d91e34bd0075b
chajus1 <chajus1@googlemail.com>
2023-06-30 16:58:25 +0100

    commented out the code that caused
    Error CS8370 Feature 'default interface implementation' is not available in C# 7.3. Please use language version 8.0 or greater.
    
    error

---
+++ ScaryClown/IClown.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ScaryClown
+{
+    interface IClown
+    {
+        string FunnyThingIHave { get; }
+        void Honk();
+
+        //protected static int Random random = new Random();
+
+        //private static int carCapacity = 12;
+
+        //public static int CarCapacity
+        //{
+        //    get { return carCapacity; }
+        //    set
+        //    {
+        //        if (value > 10) carCapacity = value;
+        //        else Console.Error.WriteLine($"Warning: Car capacity {value} is too small");
+        //    }
+        //}
+
+        //public static string ClownCarDescription()
+        //{
+        //    return $"A clown car with {random.Next(carCapacity / 2, carCapacity)} clowns";
+        //}
+    }
+}
